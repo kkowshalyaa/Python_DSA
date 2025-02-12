9eaef4bb6654ae7f2c8dc067c333296b552a38aa
Kowshalyaa Kumaraguru <76553801+kkowshalyaa@users.noreply.github.com>
2025-02-12 22:37:04 +0530

    Create ValidAnagram.py

---
+++ String/ValidAnagram.py
@@ -0,0 +1,22 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Anagram - Characters and frequency of characters should be same.
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return self.stringDict(s) == self.stringDict(t)
+
+    def stringDict(self, str_1):
+        op_dict = dict()
+        for s in str_1:
+            if s not in op_dict.keys():
+                op_dict[s] = 1
+            elif s in op_dict.keys():
+                op_dict[s] += 1
+        return op_dict
