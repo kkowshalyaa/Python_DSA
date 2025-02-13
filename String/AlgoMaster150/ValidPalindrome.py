d15d97193f9a09573fa8ecf4afe14249d5d2a954
Kowshalyaa Kumaraguru <76553801+kkowshalyaa@users.noreply.github.com>
2025-02-13 22:50:25 +0530

    Create ValidPalindrome.py

---
+++ String/AlgoMaster150/ValidPalindrome.py
@@ -0,0 +1,34 @@
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+https://leetcode.com/problems/valid-palindrome/description/
+
+'''
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_str = self.stripStr(s)
+        i, j = 0, len(cleaned_str) - 1
+        while i<j:
+            if cleaned_str[i] != cleaned_str[j]:
+                return False
+            else:
+                i, j = i+1, j-1
+        return True
+    
+    def stripStr(self, temp_s):
+        return "".join(c.lower() for c in temp_s if c.isalnum())
